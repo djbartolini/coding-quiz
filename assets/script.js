cc8343428d002deb5041bbfa65bce97fc8c60cdc
Daniel Bartolini <djb3149@gmail.com>
2022-12-03 17:08:36 -0500

    added answer bank

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
-var main = document.body.main;
-var buttonEl = document.querySelector("buttons");
-var timerEl = document.getElementById("timer");
 var h2El = document.getElementById("h2");
+var startEl = document.getElementById("start")
+var button1El = document.getElementById("button1");
+var button2El = document.getElementById("button2");
+var button3El = document.getElementById("button3");
+var button4El = document.getElementById("button4");
+var timerEl = document.getElementById("timer");
 var pEl = document.getElementById("p");
 var index = 0;
 
 var timeLeft = 75;
-
-
 
 var questions = [
     "Welcome to the Coding Quiz!",
@@ -18,36 +19,90 @@ var questions = [
     "Question 5?"
 ];
 
-
+var optionA = [
+    "1a", "2a", "3a", "4a", "5a"
+];
+var optionB = [
+    "1a", "2b", "3b", "4b", "5b"
+];
+var optionC = [
+    "1c", "2c", "3c", "4c", "5c"
+];
+var optionD = [
+    "1d", "2d", "3d", "4d", "5d"
+];
 
 // CLICK 'START' TO START THE QUIZ ////////////
 
-// var start = function() {
-//     startEl.textContent = start;
-// }
-// startEl.textContent = "Start";
-//document.body.main.appendChild(clickStart);
+button1El.style.display = "none";
+button2El.style.display = "none";
+button3El.style.display = "none";
+button4El.style.display = "none";
 
-// clickStart.textContent = "Start";
+
+var advance = function() {
+    if (index === 0) {
+        pEl.style.display = "block";
+        startEl.style.display = "block";
+        index++;
+        startQuiz();
+    } else {
+        index++;
+        startQuiz();
+    }
+}
 
 ///////////////////////////////////////////////
 
 
 
 // ADVANCE QUESTIONS /////////////
-var displayQuestion = function() {
-    h2El.textContent = questions[index];
+
+
+
+var startQuiz = function() {
     if (index > 0) {
+        h2El.textContent = questions[index];
         pEl.style.display = "none";
+        startEl.style.display = "none";
+        button1El.style.display = "block";
+        button2El.style.display = "block";
+        button3El.style.display = "block";
+        button4El.style.display = "block";      
+    }
+
+    if (index === 1) {
+        button1El.textContent = optionA[index - 1];
+        button2El.textContent = optionB[index - 1];
+        button3El.textContent = optionC[index - 1];
+        button4El.textContent = optionD[index - 1];
+    }
+    if (index === 2) {
+        button1El.textContent = optionA[index - 1];
+        button2El.textContent = optionB[index - 1];
+        button3El.textContent = optionC[index - 1];
+        button4El.textContent = optionD[index - 1];
+    }
+    if (index === 3) {
+        button1El.textContent = optionA[index - 1];
+        button2El.textContent = optionB[index - 1];
+        button3El.textContent = optionC[index - 1];
+        button4El.textContent = optionD[index - 1];
+    }
+    if (index === 4) {
+        button1El.textContent = optionA[index - 1];
+        button2El.textContent = optionB[index - 1];
+        button3El.textContent = optionC[index - 1];
+        button4El.textContent = optionD[index - 1];
+    }
+    if (index === 5) {
+        button1El.textContent = optionA[index - 1];
+        button2El.textContent = optionB[index - 1];
+        button3El.textContent = optionC[index - 1];
+        button4El.textContent = optionD[index - 1];
     }
 }
 
-var advance = function() {
-    if (index < questions.length - 1) {
-        index ++;
-        displayQuestion();
-    }
-}
 
 //////////////////////////////////
 
@@ -81,5 +136,13 @@ function displayTimer() {
 /////////////////////////////////
 
 startEl.addEventListener("click", advance);
+
+
+
+
+
+
+
+
 
 
