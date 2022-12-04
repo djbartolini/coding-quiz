d626d465a174c61b031cd2a675553584f8c6b271
Daniel Bartolini <djb3149@gmail.com>
2022-12-03 19:00:47 -0500

    added game over function & timer functionality

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,9 @@ var button2El = document.getElementById("button2");
 var button3El = document.getElementById("button3");
 var button4El = document.getElementById("button4");
 var timerEl = document.getElementById("timer");
+var formEl = document.getElementById("form");
+var submitEl = document.getElementById("submit");
+var highScoresEl = document.getElementById("high-scores");
 var pEl = document.getElementById("p");
 var index = 0;
 
@@ -23,7 +26,7 @@ var optionA = [
     "1a", "2a", "3a", "4a", "5a"
 ];
 var optionB = [
-    "1a", "2b", "3b", "4b", "5b"
+    "1b", "2b", "3b", "4b", "5b"
 ];
 var optionC = [
     "1c", "2c", "3c", "4c", "5c"
@@ -32,13 +35,17 @@ var optionD = [
     "1d", "2d", "3d", "4d", "5d"
 ];
 
-// CLICK 'START' TO START THE QUIZ ////////////
 
 button1El.style.display = "none";
 button2El.style.display = "none";
 button3El.style.display = "none";
 button4El.style.display = "none";
+timerEl.style.display = "none";
+formEl.style.display = "none";
+highScoresEl.style.display = "none";
 
+
+// CLICK 'START' TO START THE QUIZ ////////////
 
 var advance = function() {
     if (index === 0) {
@@ -58,8 +65,6 @@ var advance = function() {
 
 // ADVANCE QUESTIONS /////////////
 
-
-
 var startQuiz = function() {
     if (index > 0) {
         h2El.textContent = questions[index];
@@ -70,47 +75,68 @@ var startQuiz = function() {
         button3El.style.display = "block";
         button4El.style.display = "block";      
     }
-
-    if (index === 1) {
+     if (index === 1) {
         button1El.textContent = optionA[index - 1];
         button2El.textContent = optionB[index - 1];
         button3El.textContent = optionC[index - 1];
         button4El.textContent = optionD[index - 1];
+        button1El.addEventListener("click", advance);
+        button2El.addEventListener("click", deduct);
+        button3El.addEventListener("click", deduct);
+        button4El.addEventListener("click", deduct);
     }
     if (index === 2) {
         button1El.textContent = optionA[index - 1];
         button2El.textContent = optionB[index - 1];
         button3El.textContent = optionC[index - 1];
         button4El.textContent = optionD[index - 1];
+        button1El.addEventListener("click", advance);
+        button2El.addEventListener("click", deduct);
+        button3El.addEventListener("click", deduct);
+        button4El.addEventListener("click", deduct);
     }
     if (index === 3) {
         button1El.textContent = optionA[index - 1];
         button2El.textContent = optionB[index - 1];
         button3El.textContent = optionC[index - 1];
         button4El.textContent = optionD[index - 1];
+        button1El.addEventListener("click", deduct);
+        button2El.addEventListener("click", deduct);
+        button3El.addEventListener("click", deduct);
+        button4El.removeEventListener("click", deduct);
+        button4El.addEventListener("click", advance);
     }
     if (index === 4) {
         button1El.textContent = optionA[index - 1];
         button2El.textContent = optionB[index - 1];
         button3El.textContent = optionC[index - 1];
         button4El.textContent = optionD[index - 1];
+        button1El.addEventListener("click", deduct);
+        button2El.addEventListener("click", deduct);
+        button3El.removeEventListener("click", deduct);
+        button3El.addEventListener("click", advance);
+        button4El.addEventListener("click", deduct);
     }
     if (index === 5) {
         button1El.textContent = optionA[index - 1];
         button2El.textContent = optionB[index - 1];
         button3El.textContent = optionC[index - 1];
         button4El.textContent = optionD[index - 1];
+        button1El.removeEventListener("click", deduct);
+        button1El.addEventListener("click", advance);
+        button2El.addEventListener("click", deduct);
+        button3El.addEventListener("click", deduct);
+        button4El.addEventListener("click", deduct);
+    }
+    if (index === 6) {
+        gameOver();
     }
 }
 
-
 //////////////////////////////////
 
 
 
-// GAME OVER /////////////////////
-
-//////////////////////////////////
 
 
 
@@ -120,29 +146,43 @@ function countdown() {
     var timeInterval = setInterval(function() {
         timeLeft--;
         displayTimer();
-        if (timeLeft === 0) {
+        if (timeLeft === 0 || index > 5) {
             clearInterval(timeInterval);
-            // time up! message
         }
     }, 1000);
 }
 
 function displayTimer() {
+    timerEl.style.display = "block";
     timerEl.textContent = timeLeft;
 }
 
+function deduct() {
+    timeLeft = timeLeft - 15;
+}
 
 
 /////////////////////////////////
 
+// GAME OVER /////////////////////
+
+var gameOver = function() {
+    h2El.textContent = "All Done!"
+    button1El.style.display = "none";
+    button2El.style.display = "none";
+    button3El.style.display = "none";
+    button4El.style.display = "none";
+    formEl.style.display = "block";
+    submitEl.addEventListener("click", function(event) {
+        event.preventDefault();
+        var initials = document.querySelector("#initials").value;
+        localStorage.setItem("initials", initials);
+    })
+}
+
+
+
+//////////////////////////////////
+
 startEl.addEventListener("click", advance);
-
-
-
-
-
-
-
-
-
-
+startEl.addEventListener("click", countdown);
